c560128dfdc07b59f833a8d3be562f6ddfa89d64
Wade Flaming <wflaming@gmail.com>
2013-11-30 17:31:48 -0600

    Initial commit

---
+++ StartingProject/src/com/davidgassner/plainolnotes/data/NoteItem.java
@@ -0,0 +1,43 @@
+package com.davidgassner.plainolnotes.data;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class NoteItem {
+
+	private String key;
+	private String text;
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public static NoteItem getNew() {
+		
+		Locale locale = new Locale("en_US");
+		Locale.setDefault(locale);
+		
+		String pattern = "yyyy-MM-dd HH:mm:ss Z";
+		SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+		String key = formatter.format(new Date());	
+		
+		NoteItem note = new NoteItem();
+		note.setKey(key);
+		note.setText("");
+		return note;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getText();
+	}
+}
